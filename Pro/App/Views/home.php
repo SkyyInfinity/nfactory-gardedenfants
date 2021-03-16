66ea9d5056bd6ae26c0213b786a2b027036d0621
@theomaudet03 <theomaudet03@gmail.com>
2021-03-16 13:52:08 +0100

    wesh

--- Pro/App/Views/home.php
+++ Pro/App/Views/home.php
@@ -1,1 +1,47 @@
 <h1>PARTIE PRO</h1>
+
+<section>
+    <div class="title">
+        <p>kid'oma pro !</p>
+    </div>
+    <div>
+        <div>
+            <div class="title">
+                <p>Vous étes gérant d'une créche ?</p>
+            </div>
+            <div class="img pos1"></div>
+            <div class="text">
+                <p>
+                    Sweet roll fruitcake cookie. Tootsie roll jelly-o toffee marshmallow 
+                    tootsie roll. Cupcake marzipan lollipop. Caramels brownie pudding. Chupa
+                    chups danish dragée biscuit lemon drops. Wafer lollipop soufflé. Cupcake
+                    jelly-o icing pudding jelly-o danish candy marshmallow cookie.
+                </p>
+            </div>
+            <div class="button">Créer votre compte pro</div>
+        </div>
+
+        <div>
+            <div class="title">
+                <p>Vous étes assitante maternelle ?</p>
+            </div>
+            <div class="img pos2"></div>
+            <div class="text">
+                <p>
+                    Sweet roll fruitcake cookie. Tootsie roll jelly-o toffee marshmallow 
+                    tootsie roll. Cupcake marzipan lollipop. Caramels brownie pudding. Chupa
+                    chups danish dragée biscuit lemon drops. Wafer lollipop soufflé. Cupcake
+                    jelly-o icing pudding jelly-o danish candy marshmallow cookie.
+                </p>
+            </div>
+            <div class="button">Créer votre compte pro</div>
+            <div class="title">
+                <p>Vous avez déja un compte ?</p>
+            </div>
+            <div class="button">Se connecter</div>
+        </div>
+    </div>
+</section>
+
+<img src="./Public/assets/img/svg/house.svg" alt="House">
+<img src="./Public/assets/img/svg/woman.svg" alt="House">
