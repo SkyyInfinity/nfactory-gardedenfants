287c8d044b8bd8f50d137aaf5a5f71d380294f6e
imjustlucas <bellierlucas.pro@gmail.com>
2021-03-16 14:23:42 +0100

    Titre et nav bar

--- Pro/App/Views/default.php
+++ Pro/App/Views/default.php
@@ -10,26 +10,26 @@
 	<!-- Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/css/style.min.css">
-    <title>title | <?php if(isset($title)){echo $title;}else{echo 'undefined';} ?></title>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./Public/assets/css/reset.css">
+    <link rel="stylesheet" href="./Public/assets/css/main.css">
+
+
+    <title><?php if(isset($title)){echo $title;}else{echo 'undefined';} ?></title>
 </head>
 
 <body>
-    <!-- LOADER -->
-    <div id="loader">
-        <img src="./assets/img/loader.gif" alt="loading image">
-    </div>
     <!-- HEADER -->
-    <header id="l-header">
-		<div class="wrap">
-            <nav>
-                <ul>
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Connexion</a></li>
-                    <li><a href="#">Inscription</a></li>
-                    <li><a href="../">Partie Particulier</a></li>
-                </ul>
-            </nav>
+    <header id="proHeader">
+        <nav class="proNav">
+            <ul>
+                <li><a href="../">Partie Particulier</a></li>
+            </ul>
+        </nav>
+
+        <div class="logoPro">
+            <img src=".\Public\assets\img\logo_without_text.svg" alt="" >            
         </div>
     </header>
 
@@ -46,6 +46,6 @@
     <!-- JQUERY -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- JAVASCRIPT -->
-    <script src="./assets/js/loader.js" type="text/javascript" charset="utf-8"></script>
+    <script src="./Public/assets/js/loader.js" type="text/javascript" charset="utf-8"></script>
 </body>
 </html>
--- Pro/App/Views/home.php
+++ Pro/App/Views/home.php
@@ -1,1 +1,15 @@
-<h1>PARTIE PRO</h1>
+<?php $title = "Kid'Oma Profesionnel" ?>
+
+<h1 class="titlePage">kid'oma pro !</h1>
+
+<div class="pagecontent">
+
+    <div class="leftSide">
+        <h2>Crèche</h2>
+    </div>
+
+    <div class="rightSide">
+        <h2>Assistance maternelle</h2>
+    </div>
+
+</div>
