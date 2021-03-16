b67008d3ce367bb5b6ebb15d92e8002773e33ed6
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2021-03-16 11:09:53 +0100

    add nav

--- App/Views/default.php
+++ App/Views/default.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./Public/assets/css/style.min.css">
-    <title>title | <?php if(isset($title)){echo $title;}else{echo 'undefined';} ?></title>
+    <title>Kid'oma | <?php if(isset($title)){echo $title;}else{echo 'undefined';} ?></title>
 </head>
 
 <body>
@@ -27,6 +27,9 @@
                     <li><a href="./">Accueil</a></li>
                     <li><a href="login">Connexion</a></li>
                     <li><a href="signup">Inscription</a></li>
+                    <li class="logo">
+                        <img src="./Public/assets/img/logo_with_text.svg" alt="">
+                    </li>
                 </ul>
             </nav>
         </div>
---
+++ App/Views/home.php
@@ -0,0 +1,1 @@
+<?php $title = 'Accueil'; ?>
