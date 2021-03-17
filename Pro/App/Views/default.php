b0b1628a0c5dbb9c68ef61d053aca00ecb2d8906
@theomaudet03 <theomaudet03@gmail.com>
2021-03-17 12:03:20 +0100

    page pro accueil

--- Pro/App/Views/default.php
+++ Pro/App/Views/default.php
@@ -9,9 +9,11 @@
     <meta name="keywords" content="html,css,formation">
 	<!-- Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./Public/assets/css/style.min.css">
     <link rel="stylesheet" href="./Public/assets/css/style.css">
+    <link rel="stylesheet" href="./Public/assets/css/main.css">
     <title>title | <?php if(isset($title)){echo $title;}else{echo 'undefined';} ?></title>
 </head>
 
@@ -47,5 +49,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- JAVASCRIPT -->
     <script src="./Public/assets/js/loader.js" type="text/javascript" charset="utf-8"></script>
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/0e818bc5f3.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- Pro/App/Views/home.php
+++ Pro/App/Views/home.php
@@ -1,46 +1,55 @@
 <?php $title = "Kid'Oma Profesionnel" ?>
 
-<h1>PARTIE PRO</h1>
-
 <section id="main">
     <div class="titlemain">
         <p>kid'oma pro !</p>
     </div>
-    <div>
-        <div>
+    <div class="box_categorie">
+        <div class="box">
             <div class="title">
                 <p>Vous étes gérant d'une créche ?</p>
             </div>
-            <div class="img pos1"><img src="./Public/assets/img/svg/house.svg" alt="House"></div>
-            <div class="text">
-                <p>
-                    Sweet roll fruitcake cookie. Tootsie roll jelly-o toffee marshmallow 
-                    tootsie roll. Cupcake marzipan lollipop. Caramels brownie pudding. Chupa
-                    chups danish dragée biscuit lemon drops. Wafer lollipop soufflé. Cupcake
-                    jelly-o icing pudding jelly-o danish candy marshmallow cookie.
-                </p>
+            <div class="content_box1">
+                <div class="img pos1"><img src="./Public/assets/img/svg/house.svg" alt="House"></div>
+                <div class="text">
+                    <p>
+                        Sweet roll fruitcake cookie. Tootsie roll jelly-o toffee marshmallow 
+                        tootsie roll. Cupcake marzipan lollipop. Caramels brownie pudding. Chupa
+                        chups danish dragée biscuit lemon drops. Wafer lollipop soufflé. Cupcake
+                        jelly-o icing pudding jelly-o danish candy marshmallow cookie.
+                    </p>
+                </div>
+                <div class="button"><button><i class="fas fa-arrow-right"></i> Créer votre compte pro <i class="fas fa-user"></i></button></div>
             </div>
-            <div class="button">Créer votre compte pro</div>
         </div>
 
-        <div>
-            <div class="title">
-                <p>Vous étes assitante maternelle ?</p>
+        <div class="box">
+            <div class="content1">
+                <div class="content1right">
+                    <div class="title">
+                        <p>Vous étes assitante maternelle ?</p>
+                    </div>
+                    <div class="text">
+                        <p>
+                            Sweet roll fruitcake cookie. Tootsie roll jelly-o toffee marshmallow 
+                            tootsie roll. Cupcake marzipan lollipop. Caramels brownie pudding. Chupa
+                            chups danish dragée biscuit lemon drops. Wafer lollipop soufflé. Cupcake
+                            jelly-o icing pudding jelly-o danish candy marshmallow cookie.
+                        </p>
+                    </div>
+                    <div class="button"><button><i class="fas fa-arrow-right"></i> Créer votre compte pro <i class="fas fa-user"></i></button></div>
+                </div>
+                <div class="content1left img pos2">
+                    <img src="./Public/assets/img/svg/woman.svg" alt="woman">
+                </div>
             </div>
-            <div class="img pos2"><img src="./Public/assets/img/svg/woman.svg" alt="woman"></div>
-            <div class="text">
-                <p>
-                    Sweet roll fruitcake cookie. Tootsie roll jelly-o toffee marshmallow 
-                    tootsie roll. Cupcake marzipan lollipop. Caramels brownie pudding. Chupa
-                    chups danish dragée biscuit lemon drops. Wafer lollipop soufflé. Cupcake
-                    jelly-o icing pudding jelly-o danish candy marshmallow cookie.
-                </p>
-            </div>
-            <div class="button">Créer votre compte pro</div>
             <div class="title">
                 <p>Vous avez déja un compte ?</p>
             </div>
-            <div class="button">Se connecter</div>
+            <div class="login">
+                <div class="button loginbtn"><button><i class="fas fa-arrow-right"></i> Se connecter <i class="fas fa-user"></i></button></div>
+                <div><img src="./Public/assets/img/svg/profil.svg" alt=""></div>
+            </div>
         </div>
     </div>
 </section>
