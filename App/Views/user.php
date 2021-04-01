9ecbd0373996ebe48a1eaa044dcac8e9e3835d08
Sky Infinity <dylan.hautecoeur@gmail.com>
2021-04-01 19:43:16 +0200

    Add geolocalisation with input

--- App/Views/user.php
+++ App/Views/user.php
@@ -28,9 +28,14 @@ $title = 'Mon espace'; ?>
             <div class="map">
                 <h2 class="second-title">Nourisse et crêche à proximité</h2>
                 <div class="geolocalisation">
-                    <button class="btn-submit btn-orange" id="geolocMe">Trouver à proximité</button>
-                    <input type="search" name="geo" id="geo">
-                    <button class="btn-submit btn-orange" id="searchGeo"><i class="fas fa-search"></i></button>
+                    <button class="btn-submit btn-orange" id="geolocMe">Me géolocaliser</button>
+                    <span>Ou</span>
+                    <input type="search" name="geo" id="geo" placeholder="Taper votre adresse ici">
+                    <div id="geolist">
+                        <tr>
+
+                        </tr>
+                    </div>
                 </div>
                 <div id='map' style='width: 100%; height: 600px;'></div>
             </div>
@@ -44,7 +49,7 @@ $title = 'Mon espace'; ?>
                         <p><?php if($child->age <= 1){echo $child->age . ' an';}else{echo $child->age . ' ans';} ?></p>
                     </div>
                     <div class="btn">
-                        <a href="reserve/<?= $child->id ?>" class="btn btn-orange">Reservé pour <?= $child->name ?></a>
+                        <a href="reserve/<?= $child->id ?>" class="btn btn-orange">Reserver pour <?= $child->name ?></a>
                     </div>
                 </li>
                 <?php endforeach ?>
