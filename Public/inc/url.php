f0a2e087d2d52773972cbd155df95b7877d315f7
imjustlucas <bellierlucas.pro@gmail.com>
2021-04-02 09:22:34 +0200

    test

--- Public/inc/url.php
+++ Public/inc/url.php
@@ -1,7 +1,2 @@
 <?php 
-<<<<<<< HEAD
 define('URL', 'http://localhost/Projet/GardeEnfant/nfactory-gardedenfants/');
-=======
-define('URL', 'http://localhost/Nfactory/projet/kidoma/');
-
->>>>>>> 9ecbd0373996ebe48a1eaa044dcac8e9e3835d08
